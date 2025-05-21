a68b47b9c87f8fcf59ec4690741bd1c45b890490
peteboonthab <0287763@schoolsnet.act.edu.au>
2025-05-22 00:39:32 +1000

    Add files via upload

---
+++ Form6.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Original1
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+        private void Form6_Load_1(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightBlue;
+            textBox1.BackColor = Color.LightYellow;
+            textBox3.BackColor = Color.LightYellow;
+            textBox1.Text = "This lesson is about using \"If Else Statement \"" + Environment.NewLine + "" + Environment.NewLine +
+            "An if-else statement is like making a decision, it tells the computer:" + Environment.NewLine + "" + Environment.NewLine +
+            "if it is true, do this" + Environment.NewLine +
+            "else (if it is not true) do this" + Environment.NewLine + "" + Environment.NewLine +
+            "Example:" + Environment.NewLine + "" + Environment.NewLine +
+            "int number = 10;" + Environment.NewLine +
+            "if (number > 5) {" + Environment.NewLine +
+            "    Console.WriteLine(\"The number is greater than 5\");" + Environment.NewLine +
+            "} else {" + Environment.NewLine +
+            "    Console.WriteLine(\"The number is not greater than 5\");" + Environment.NewLine +
+            "}" + Environment.NewLine + "" + Environment.NewLine +"Result: The number is greater than 5";
+            textBox3.Text = "Question 1" + Environment.NewLine + Environment.NewLine +
+    "What will be printed to the screen?" + Environment.NewLine +
+    "int number = 7;" + Environment.NewLine +
+    "if (number > 10)" + Environment.NewLine +
+    "{" + Environment.NewLine +
+    "    Console.WriteLine(\"Greater than 10\");" + Environment.NewLine +
+    "}" + Environment.NewLine +
+    "else" + Environment.NewLine +
+    "{" + Environment.NewLine +
+    "    Console.WriteLine(\"10 or less\");" + Environment.NewLine +
+    "}" + Environment.NewLine + Environment.NewLine +
+    "Answer:";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+
+            this.Hide();
+        }
+        private void btnGo_Click(object sender, EventArgs e)
+
+        {
+
+            string[] allLines = textBox2.Text.Split('\n');
+
+
+
+
+
+            foreach (string text in allLines)
+
+            {
+
+                string trimmed = text.Trim(); // Remove extra spaces or newlines
+
+                if (trimmed != "10 or less")
+
+                {
+
+                    lblCheckResult.Text = "Incorrect!";
+                    lblCheckResult.ForeColor = Color.Red;
+                }
+                else
+                {
+
+
+                    lblCheckResult.Text = "Correct!";
+                    lblCheckResult.ForeColor = Color.Green;
+                }
+            }
+        }
+
+
+    }
+}
